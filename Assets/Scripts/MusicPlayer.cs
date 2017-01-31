d0e2b4225bc5738a9c1a947c0079145f90d4af39
Rurouni <hafrana@gmail.com>
2017-01-31 21:16:58 +0000

    new assets added

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour 
+{
+	static MusicPlayer instance = null;
+	public bool loop;
+
+	void Awake() 
+	{
+		
+    	if (instance != null) 
+    	{
+        	Destroy(gameObject);
+    	} else 
+    	{
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+    	}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
